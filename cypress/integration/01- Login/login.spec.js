0ddc141c8c2484f30e80b97f19d955fa3467d29e
Saul Mendoza - Home <saulmendoza08@gmail.com>
2023-12-05 19:53:22 -0300

    terminado ejercicio LOGIN

--- cypress/integration/01- Login/login.spec.js
+++ cypress/integration/01- Login/login.spec.js
@@ -5,13 +5,19 @@ context('Login',()=>{
     
     //login
     it('Test #1',()=>{
-        cy.get('#85ca2f1f-fd2a-15fc-e180-fd8a46b35199').type("User{enter}");
-        cy.get('#4306b2fb-183c-7b2f-9d16-a0c9b28e62c4').type("pwd{enter}");
+        cy.get('input[name="UserName"]').type("User{enter}");
+        cy.get('input[name="Password"]').type("pwd{enter}");
         cy.get('#login').click();
+        
         cy.get('#loginstatus')
             .invoke('text')
             .then(texto => {
-                console.log(texto)
+                if (texto=="Welcome, User!") {
+                    console.log("Contraseña correcta!")
+                }else{
+                    console.log("Contraseña incorrecta!")
+                }
             })
+        
     })
 })
